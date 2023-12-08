00e3f7e76997ae297676a53effeb6a481663f874
Artur Karczmarczyk <artur@ideaspot.pl>
2023-12-08 12:20:53 +0100

    routing and components skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {StartComponent} from "./start/start.component";
+import {ItemsComponent} from "./items/items.component";
+import {UsersComponent} from "./users/users.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "items", component: ItemsComponent },
+  { path: "users", component: UsersComponent },
+  { path: "", pathMatch: "full", component: StartComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
