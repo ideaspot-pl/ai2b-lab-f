4a0a9ed77aa4482a05aeda4bf95f1f03366c29c9
Artur Karczmarczyk <artur@ideaspot.pl>
2023-12-08 12:40:04 +0100

    visibility of links

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {AuthToken} from "./auth-token";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(
+    private jwtHelper: JwtHelperService,
+  ) { }
 
-  constructor() { }
+  public isAuthenticated(): boolean {
+    return !this.jwtHelper.isTokenExpired();
+  }
+
+  public isAdmin(): boolean {
+    const token = this.jwtHelper.decodeToken() as AuthToken;
+    if (!this.isAuthenticated()) {
+      return false;
+    }
+    return token && token.roles && token.roles.includes('ADMIN');
+  }
 }
